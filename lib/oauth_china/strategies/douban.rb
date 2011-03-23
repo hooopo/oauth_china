4475d6d5626f79bbf9eb8725e46e034c513a4ca1
Hooopo <hoooopo@gmail.com>
2011-03-23 18:41:00 +0800

    豆瓣发送广播功能

--- lib/oauth_china/strategies/douban.rb
+++ lib/oauth_china/strategies/douban.rb
@@ -30,10 +30,10 @@ module OauthChina
     end
 
     def add_status(content, options = {})
-      self.post("/miniblog/saying", %Q{
+      self.post("http://api.douban.com/miniblog/saying", %Q{
         <?xml version='1.0' encoding='UTF-8'?>
         <entry xmlns:ns0="http://www.w3.org/2005/Atom" xmlns:db="http://www.douban.com/xmlns/">
-          <content>#{content}</content>
+        <content>#{content}</content>
         </entry>
         }, {"Content-Type" =>  "application/atom+xml"})
     end
